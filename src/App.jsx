900a5891bbc3aef07ed885f255edba68d3d8594f
kocaman14 <o.kcmn@hotmail.com>
2024-12-24 19:23:16 +0300

    figmatoreact

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+"use client"
+
+export default function App() {
+
+  return (
+<div className="overflow-hidden w-full h-full">
+  <div className="w-[708px] h-[594px] top-[246px] absolute  ml-[200px] mt-[150px]">
+<img src="./images/newlogin.png"></img>
+<input className="w-[595.3px] h-[58.85px] top-[149.01px] left-[57.51px] gap-0 border border-opacity-0" type="text" placeholder="Username" />
+<input className="w-[595.54px] h-[59.18px] top-[230.15px] left-[57.51px] gap-0 border border-opacity-0 mt-[20px]" type="text" placeholder="Password" />
+<img src="./images/login-button.png" className="w-[313.99px] h-[60.74px] top-[324.4px] left-[57.51px] mt-[20px]"></img>
+<div className="mt-[20px] flex justify-between w-[468px]">
+<p className="font-[Montserrat] text-[20px] font-semibold leading-[24.38px] text-left  decoration-skip-none text-[rgba(0,0,0,0.7)]">Forgot password</p>  
+<p className="font-[Montserrat] text-[20px] font-semibold leading-[24.38px] text-left  decoration-skip-none text-[rgba(0,0,0,0.7)]">Register</p>  
+</div>
+</div>
+<img src="./images/hamburger.png" className="w-[612px] h-[612px] top-[246px] left-[1200px] gap-0 shadow-none absolute  z-10"></img>
+<img src="./images/Rectangle 12.png" className="w-[649.33px] h-[1143.68px] top-[-114.7px] left-[1260.44px] gap-0  absolute "></img>
+
+
+
+</div>
+
+  );
+}
