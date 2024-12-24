2a61cec86c5379d5826b2d92f26fbcf3cff6d628
kocaman14 <163642815+kocaman14@users.noreply.github.com>
2024-12-24 21:43:40 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@
 export default function App() {
 
   return (
-<div className="flex items-center justify-center w-[1440px] h-[873px] top-[-437px] left-[-720px] gap-0 overflow-hidden ">
-  <div className="w-[708px] h-[594px] top-[200px] absolute   mt-[150px]">
+<div className="flex items-center justify-center overflow-hidden w-[1440px] h-[873px] top-[-437px] left-[-720px] gap-0 ">
+  <div className="w-[708px] h-[594px] top-[100px] absolute   mt-[150px]">
 <img src="./images/newlogin.png"></img>
 <input className="w-[595.3px] h-[58.85px] top-[149.01px] left-[57.51px] gap-0 border border-opacity-0" type="text" placeholder="Username" />
 <input className="w-[595.54px] h-[59.18px] top-[230.15px] left-[57.51px] gap-0 border border-opacity-0 mt-[20px]" type="text" placeholder="Password" />
@@ -14,7 +14,7 @@ export default function App() {
 <p className="font-[Montserrat] text-[20px] font-semibold leading-[24.38px] text-left  decoration-skip-none text-[rgba(0,0,0,0.7)]">Register</p>  
 </div>
 </div>
-<img src="./images/hamburger.png" className="w-[612px] h-[612px] top-[200px] left-[1000px] gap-0 shadow-none absolute  z-10"></img>
+<img src="./images/hamburger.png" className="w-[612px] h-[612px] top-[130px] left-[1000px] gap-0 shadow-none absolute  z-10"></img>
 <img src="./images/Rectangle 12.png" className="w-[649.33px] h-[800px.68px] top-[90.7px] left-[1100.44px] gap-0  absolute "></img>
 
 
